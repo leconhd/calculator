08f55a2a1be588c48a0195766959bc384a02286a
Constantin <mail@constantin-weber.de>
2022-06-07 12:40:32 +0200

    fast fertig

--- calculator.js
+++ calculator.js
@@ -1,11 +1,13 @@
 let displayValue = [];
 let alsNummer = 0;
+let first = 0;
+let dieRechnung;
 
-function add(numA, numB) {
+function plus(numA, numB) {
     console.log(numA + numB);
 };
 
-function subst(numA, numB) {
+function minus(numA, numB) {
     console.log(numA - numB);
 };
 
@@ -26,11 +28,45 @@ function display(arg) {
     anzeige.innerHTML = arg;
 };
 
-function numbers() {
+//add event listeners to numbers
+function addListenerNumbers() {
     let allNumbers = document.querySelectorAll(".number");
     allNumbers.forEach(element => element.addEventListener("click",() => addNumber(element.innerHTML)));
 };
 
+//add event listener to AC
+function addListenerAC() {
+    let dasAC = document.querySelector("#ac");
+    dasAC.addEventListener("click", löschen);
+}
+
+// //add event listener to Operations, hand over first number and operation
+function addListenersOperations() {
+    let dieOperationen = document.querySelectorAll(".operate");
+    dieOperationen.forEach(element => element.addEventListener("click",() => waitForOperation(alsNummer, element.id)));
+}
+
+function waitForOperation(ersteZahl, rechnung) {
+    first = ersteZahl;
+    dieRechnung = rechnung;
+    displayValue = [];
+    alsNummer = 0;
+}
+
+//add event listener to equals
+function addEventListenerEquals() {
+    let dieGleichheit = document.querySelector(".equal");
+    dieGleichheit.addEventListener("click", () => equals(alsNummer));
+}
+
+
+function equals(dieZweite) {
+    console.log(dieRechnung + "(" + first + "," + dieZweite +")");
+    console.log(dieRechnung);
+    operate(dieRechnung,first,dieZweite);
+};
+
+//add number to display
 function addNumber(arg) {
     displayValue.push(arg);
     let stringArray = displayValue.reduce((a,b) => 
@@ -39,6 +75,17 @@ function addNumber(arg) {
     display(alsNummer);
 };
 
+//löschen 
+function löschen() {
+    displayValue = [];
+    alsNummer = 0;
+    display(alsNummer);
+}
 
-//add event listeners to numbers
-numbers();
+
+//add event listeners
+addListenerNumbers();
+addListenerAC();
+addListenersOperations();
+addEventListenerEquals();
+operate(minus,66,6);
